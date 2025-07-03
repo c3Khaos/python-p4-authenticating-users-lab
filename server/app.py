501106597855bad5f2d14c46e18688c98bc727f3
christopherchege <chrisshege35@gmail.com>
2025-07-03 22:11:16 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -47,7 +47,38 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
 
+class Login(Resource):
+    def post(self):
+        data=request.get_json()
+        username=data.get("username")
+
+        if not username:
+            return {"error":"Username is required"},400
+        
+        user=User.query.filter(username=username).first()
+        if user:
+            session["user_id"]=user.id
+            return user.to_dict(),200
+        return {"error":"User not found"},404
+    
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return '', 401
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
